a35ccfa38749ae4ed19bde259b253369864db27d
thaisartell <thaisartell@gmail.com>
2026-04-10 16:09:38 -0500

    ben's changes caught up to MVP_integration

--- include/buttons.h
+++ include/buttons.h
@@ -1,5 +1,5 @@
-#pragma once
-
+#ifndef BUTTONS_H
+#define BUTTONS_H
 
 #include <Arduino.h>
 
@@ -12,23 +12,20 @@ typedef struct {
 
 namespace buttonPins {
     extern button_t powerButton;
-    extern button_t autoHoming;
-    extern button_t triggerButton;
     extern button_t toolSelect;
+    extern button_t triggerInput;
 }
 
 typedef struct {
   bool power_pressed  = false;
   bool power_released = false;
 
-  bool item_pressed   = false;
-  bool item_released  = false;
+  bool tool_pressed   = false;
+  bool tool_released  = false;
 
   bool trigger_pressed  = false;
   bool trigger_released = false;
 
-  bool home_pressed   = false;
-  bool home_released  = false;
 } ButtonEvents_t;
 
 void buttonInit(button_t* b);
@@ -40,3 +37,10 @@ void buttonUpdate(button_t* b);
 namespace Buttons {
     void setup();
 };
+
+void ToolCycleButton(button_t* b);
+void testPowerButton(button_t* b);
+float getTriggerDepth(button_t* b);
+void triggerPulled(button_t* b);
+
+#endif
